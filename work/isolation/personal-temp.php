8c7e49973d4983d084bc352644640c04877902bc
yoru6211 <ren.33.h@gmail.com>
2020-11-16 10:01:59 +0900

    new created personal-temp.php file

---
+++ work/isolation/personal-temp.php
@@ -0,0 +1,11 @@
+<?php
+// mysql
+$dsn = 'mysql:dbname="database-name";host="hostname";charset=utf8mb4';
+$db_user = 'username';
+$db_passwd = 'password';
+
+// gmail/smtpサーバー
+$my_account = 'email';
+$my_password = 'password';
+$receive = 'email';
+?>
